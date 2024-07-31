2c6e86f26438b1117050de3b219d6a76350d1de0
Leonardo Gonçalves <leonardo.goncalves03@aluno.ifce.edu.br>
2024-07-31 19:52:57 -0300

    adicionando arquivos da atividade 03

--- Exercicios/Exercicio-03.c
+++ Exercicios/Exercicio-03.c
@@ -1,12 +1,4 @@
-Exercicio 03
-
-1-A manipulação de bits envolve a alteração de bits individuais em um número binário. Isso pode ser feito por meio de várias operações, incluindo as operações lógicas AND, OR e XOR, bem como deslocamentos de bits para a esquerda e para a direita.
-
-2-É importante para otimizar a eficiência e o desempenho do código. Ela permite que os programadores realizem operações complexas de forma rápida e eficiente, melhorando o desempenho do código e reduzindo o uso de recursos.
-
-3-As operações básicas de manipulação de bits incluem AND bit a bit, OR bit a bit, XOR bit a bit e deslocamentos de bits para a esquerda e para a direita.
-
-4- #include <stdio.h>
+ #include <stdio.h>
 
 // Função para imprimir os bits de um número
 void imprimir_bits(int num) {
@@ -58,5 +50,3 @@ int main() {
 
     return 0;
 }
-
-
